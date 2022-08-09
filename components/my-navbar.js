f220456a9c77ea5d0cef78a7f76746afccbb4e59
Chuka <b.chuka@gmail.com>
2022-08-09 15:44:53 +0800

    Navbar oruulsan. ehnii huvilbar

---
+++ components/my-navbar.js
@@ -0,0 +1,18 @@
+import { Navbar, Nav } from "react-bootstrap";
+export default () => {
+  return (
+    <Navbar className="fj-navbar fj-nav-base" bg="transparent" expand="lg">
+      <Navbar.Brand className="fj-navbar-brand">
+        <a href="#">Chuka's Blog</a>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link className="fj-navbar-item fj-navbar-link" href="/">
+            HOME
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
